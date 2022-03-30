fd29a8325eb5f3708b0d7b1763e00897d09d4ce1
McTuTuM <kostu3660@yandex.ru>
2022-03-30 17:40:19 +0300

    add_cr_table

--- logic.py
+++ logic.py
@@ -1,68 +1,143 @@
 import numpy as np
 import random
-from time import thread_time
-from math import factorial
+import copy
 
 class CheckSul:
-    def done_or_not(): #board[i][j]
-        while True:
-            b = CheckSul._creater_table()
-            # b = [[1, 3, 2, 5, 7, 9, 4, 6, 8]
-            #             ,[4, 9, 8, 2, 6, 1, 3, 7, 5]
-            #             ,[7, 5, 6, 3, 8, 4, 2, 1, 9]
-            #             ,[6, 4, 3, 1, 5, 8, 7, 9, 2]
-            #             ,[5, 2, 1, 7, 9, 3, 8, 4, 6]
-            #             ,[9, 8, 7, 4, 2, 6, 5, 3, 1]
-            #             ,[2, 1, 4, 9, 3, 5, 6, 8, 7]
-            #             ,[3, 6, 5, 8, 1, 7, 9, 2, 4]
-            #             ,[8, 7, 9, 6, 4, 2, 1, 5, 3]]
-            board = np.array(b)
-            rows = [board[i,:] for i in range(9)]
-            cols = [board[:,j] for j in range(9)]
-            sqrs = [board[i:i+3,j:j+3].flatten() for i in [0,3,6] for j in [0,3,6]]
-            cnt = 0
-            for view in np.vstack((rows,cols,sqrs)):
-                a = False
-                if len(np.unique(view)) != 9:
-                    a = True
-                else:
-                    cnt += 1
-                if cnt == 9:
-                    print(thread_time())
-                    print(b)
-                    return b
-                if a == True:
-                    break
+    def __init__(self):
+        self.game_table = []
+        self.table = []
+        self.n = 3
 
-        
-    def _creater_table():
+       
+    def _creater_table(self):
         a = []
-        b = []
-        list = [i for i in range(1, 10)]
         for i in range(9):
-            temp_list = list.copy()
-            # for j in range(9):
-            flag = False
-            cnt = 0
-            while flag == False:
-                temp  = temp_list.pop(random.randint(0, len(temp_list) - 1))
-                try:
-                    # print(tuple(b[z][9 - len(temp_list) - 1] for z in range(i)))
-                    if temp not in tuple(b[z][9 - len(temp_list) - 1] for z in range(i)):
-                        a.append(temp)
-                    else:
-                        cnt += 1
-                        temp_list.append(temp)
-                        if cnt > factorial(len(temp_list)):
-                            temp_list = list.copy()
-                            a = []
-                            cnt = 0
-                except IndexError:
-                    a.append(temp)
-                if len(temp_list) == 0:
-                    flag = True
-            b.append(a)
+            for j in range(9):
+                 a.append(int((i*3 + i/3 + j) % 9 + 1))
+            self.table.append(a)
             a = []
-        return b
+        self.mix()
 
-# done_or_not()
+    def transposing(self):
+        self.table = list(map(list, zip(*self.table)))
+
+    def swap_rows_small(self):
+        area = random.randrange(self.n)
+        line1 = random.randrange(self.n)
+        N1 = area*self.n + line1
+        line2 = random.randrange(self.n)
+        while (line1 == line2):
+            line2 = random.randrange(self.n)
+        N2 = area*self.n + line2
+        self.table[N1], self.table[N2] = self.table[N2], self.table[N1]
+
+    def swap_colums_small(self):
+        self.transposing()
+        self.swap_rows_small()
+        self.transposing()
+
+    def swap_colums_area(self):
+        self.transposing()
+        self.swap_rows_area()
+        self.transposing()
+
+    def swap_rows_area(self):
+        area1, area2 = random.sample(range(3), 2)
+        for i in range(3):
+            n1, n2 = area1 * 3 + i, area2 * 3 + i
+            self.table[n1], self.table[n2] = self.table[n2], self.table[n1]
+
+    def mix(self, amt = 10):
+        mix_func = (
+			self.swap_rows_small,
+			self.swap_colums_small,
+			self.swap_rows_area,
+			self.swap_colums_area,
+		)
+        
+        for _ in range(amt):
+            self.transposing()
+            random.choice(mix_func)()
+        self.game_table = copy.deepcopy(self.table)
+        self.diff()
+        print(self.table)
+
+    def diff(self):
+        # a = self.table.copy()
+        for i in range(self.n**2):
+            for j in range(self.n**2):
+                if self.game_table[i][j] == 0:
+                    continue
+                if random.random() > 0.7:
+                    self.game_table[i][j] = 0
+        # self.game_table = a
+        print()
+# a = CheckSul()
+# a._creater_table()
+
+
+
+
+
+
+
+
+    # def done_or_not(self): #board[i][j]
+    #     while True:
+    #         # b = [[1, 3, 2, 5, 7, 9, 4, 6, 8]
+    #         #             ,[4, 9, 8, 2, 6, 1, 3, 7, 5]
+    #         #             ,[7, 5, 6, 3, 8, 4, 2, 1, 9]
+    #         #             ,[6, 4, 3, 1, 5, 8, 7, 9, 2]
+    #         #             ,[5, 2, 1, 7, 9, 3, 8, 4, 6]
+    #         #             ,[9, 8, 7, 4, 2, 6, 5, 3, 1]
+    #         #             ,[2, 1, 4, 9, 3, 5, 6, 8, 7]
+    #         #             ,[3, 6, 5, 8, 1, 7, 9, 2, 4]
+    #         #             ,[8, 7, 9, 6, 4, 2, 1, 5, 3]]
+    #         board = np.array(self.table)
+    #         rows = [board[i,:] for i in range(9)]
+    #         cols = [board[:,j] for j in range(9)]
+    #         sqrs = [board[i:i+3,j:j+3].flatten() for i in [0,3,6] for j in [0,3,6]]
+    #         cnt = 0
+    #         for view in np.vstack((rows,cols,sqrs)):
+    #             a = False
+    #             if len(np.unique(view)) != 9:
+    #                 a = True
+    #             else:
+    #                 cnt += 1
+    #             if cnt >= 26:
+    #                 print(thread_time())
+    #                 print(self.table)
+    #                 return self.table
+    #             if a == True:
+    #                 break
+
+
+#         a = []
+#         b = []
+#         list = [i for i in range(1, 10)]
+#         for i in range(9):
+#             temp_list = list.copy()
+#             # for j in range(9):
+#             flag = False
+#             cnt = 0
+#             while flag == False:
+#                 temp  = temp_list.pop(random.randint(0, len(temp_list) - 1))
+#                 try:
+#                     # print(tuple(b[z][9 - len(temp_list) - 1] for z in range(i)))
+#                     if temp not in tuple(b[z][9 - len(temp_list) - 1] for z in range(i)):
+#                         a.append(temp)
+#                     else:
+#                         cnt += 1
+#                         temp_list.append(temp)
+#                         if cnt > factorial(len(temp_list)):
+#                             temp_list = list.copy()
+#                             a = []
+#                             cnt = 0
+#                 except IndexError:
+#                     a.append(temp)
+#                 if len(temp_list) == 0:
+#                     flag = True
+#             b.append(a)
+#             a = []
+#         return b
+# CheckSul.done_or_not()
--- sudoku.py
+++ sudoku.py
@@ -13,8 +13,11 @@ class MainWindow():
         self.wight = 950
         self.game_over = False
         self.start_game = False
+        self.press = False
         self.game_mode = 0
         self.table = []
+        self.fin_table = []
+        self.key, self.i, self.j = 0, 0, 0
         self.core()
         
 
@@ -30,6 +33,10 @@ class MainWindow():
                     self.click(event.pos[0], event.pos[1], event.type)
                 if event.type == pygame.QUIT:
                     self.exit()
+                if self.press == True:
+                    if event.type == pygame.KEYDOWN:
+                        if event.key >= 49 and event.key <= 58:
+                                self.input_key(event.key)
                 pygame.display.flip()
             
     def click(self, x, y, event_type):
@@ -47,6 +54,7 @@ class MainWindow():
                     
                 if x > 730 and x < 880 and y > 160 and y < 240:
                     self.start_game = False
+                    sleep(1)
                     self.game_mode = 0
                     self.draw_win_before()
                 if x > 50 and x < 680 and y > 60 and y < 690:
@@ -59,34 +67,59 @@ class MainWindow():
         for i in range(1, 10):
             for j in range(1, 10):
                 if x > x_start + step * (i - 1) and x < x_start + step * i and y > y_start + step * (j - 1) and y < y_start + step * j:
-                    
+                    num = self.table[j - 1][i - 1]
+                    if num == 0:
+                        self.press = True
+                        pygame.draw.rect(self.screen, 'white', [(50, 60), (630, 630)]) # game_field
+                        self.draw_grid()
+                        self.draw_numbs()
+                        self.red_sqrt((x_start + step * (i - 1)) + 3, (y_start + step * (j - 1)) + 3, step - 5)
+                        self.i, self.j = i, j
+                        print('empty', self.fin_table[j - 1][i - 1])
                     print('__', i, j, x, y)
                     print('***', self.table[j - 1][i - 1])
                     
-                    # sur = pygame.image.load('png/1.png')
-                    # rec = sur.get_rect(center  = ((x_start + int(1/2 * step) + step * (i - 1)) + 1, ( y_start + int(1/2 * step) + step * (j - 1)) + 1))
-                    # self.screen.blit(sur, rec)
-                    # pygame.display.update
+    def input_key(self, key):
+        self.key = key - 48
+        x_start = 50
+        y_start = 60
+        step = 70
+        sur = pygame.image.load(f'png/{str(self.key)}.png')
+        rec = sur.get_rect(center  = ((x_start + int(1/2 * step) + step * (self.i - 1)) + 1, ( y_start + int(1/2 * step) + step * (self.j - 1)) + 1))
+        self.screen.blit(sur, rec)
+        pygame.display.update
+        if self.fin_table[self.j - 1][self.i - 1] == self.key:
+            self.table[self.j - 1][self.i - 1] = self.key
+        else:
+            print("nOOOOOOOOOOOOOOOOOO")
 
-    def draw_numb(self):
+        if self.fin_table == self.table:
+            print('game win')
+
+    def draw_numbs(self):
         x_start = 50
         y_start = 60
         step = 70
         for i in range(1, 10):
             for j in range(1, 10):
                 num = self.table[j - 1][i - 1]
-                sur = pygame.image.load(f'png/{str(num)}.png')
-                rec = sur.get_rect(center  = ((x_start + int(1/2 * step) + step * (i - 1)) + 1, ( y_start + int(1/2 * step) + step * (j - 1)) + 1))
-                self.screen.blit(sur, rec)
-                pygame.display.update
+                if num != 0:
+                    sur = pygame.image.load(f'png/{str(num)}.png')
+                    rec = sur.get_rect(center  = ((x_start + int(1/2 * step) + step * (i - 1)) + 1, ( y_start + int(1/2 * step) + step * (j - 1)) + 1))
+                    self.screen.blit(sur, rec)
+                    pygame.display.update
 
     def start(self):
+        self.press = False
         self.start_game = True
         threading.Thread(target=self.timer, daemon= True).start()
-        self.table = CheckSul.done_or_not()
+        check_sul = CheckSul()
+        check_sul._creater_table()
+        self.table = check_sul.game_table      
+        self.fin_table = check_sul.table  
         self.draw_win_after()
         self.draw_grid()
-        self.draw_numb()
+        self.draw_numbs()
 
     def timer(self): 
         f1 = pygame.font.Font(None, 48)
@@ -100,6 +133,7 @@ class MainWindow():
                         self.screen.blit(f1.render(f"{m1}{m2}:{s1}{s2}", True, 'black'), (760, 265))        
                         pygame.display.update()
                         sleep(1)
+        return
             
 
     def draw_grid(self):
@@ -107,7 +141,7 @@ class MainWindow():
         for i in range(1, 11):
             w = 3
             if (i - 1) % 3 == 0:
-                w = 10
+                w = 7
             x0 = 50
             x1 = 680
             y0 = i * step - 10         
@@ -121,6 +155,7 @@ class MainWindow():
     def draw_line(self, x0, x1, y0, y1, x0_1, x1_1, y0_1, y1_1, w):
         pygame.draw.line(self.screen, 'black', (x0, y0), (x1, y1), w)
         pygame.draw.line(self.screen, 'black', (x0_1, y0_1), (x1_1, y1_1), w)
+
     def game_mode_set(self):
         f1 = pygame.font.Font(None, 48)
         self.game_mode += 1
@@ -140,6 +175,7 @@ class MainWindow():
     def draw_win_after(self):
         f1 = pygame.font.Font(None, 48)
         f2 = pygame.font.Font(None, 38)
+        pygame.draw.rect(self.screen, 'white', [(50, 60), (630, 630)])
         pygame.draw.rect(self.screen, 'white', [(730, 60), (150, 80)]) #buttom start game
         self.screen.blit(f1.render("Пауза", True, 'black'), (745, 80))
         pygame.draw.rect(self.screen, 'white', [(730, 160), (150, 80)])  #bottom_pause
@@ -150,8 +186,8 @@ class MainWindow():
     def draw_win_before(self):
         f1 = pygame.font.Font(None, 48)
         self.screen = pygame.display.set_mode((self.wight, self.height))
-        self.screen.fill(THECOLORS['grey'])
-        pygame.draw.rect(self.screen, 'white', [(50, 60), (630, 630)])
+        self.screen.fill((75,75,75))
+        pygame.draw.rect(self.screen, (200,200,200), [(50, 60), (630, 630)])
         pygame.draw.rect(self.screen, 'white', [(730, 60), (150, 80)]) #buttom start game
         self.screen.blit(f1.render("Начать", True, 'black'), (745, 80))
         self.game_mode_set()
@@ -159,6 +195,9 @@ class MainWindow():
         pygame.draw.rect(self.screen, 'white', [(730, 610), (150, 80)])   #buttom quit
         self.screen.blit(f1.render("Выход", True, 'black'), (750, 635))
 
+    def red_sqrt(self,x1,y1,step):
+        pygame.draw.rect(self.screen, 'red', [(x1, y1), (step, step)], 3)
+        
 
 
 # class GameWindow(MainWindow):
